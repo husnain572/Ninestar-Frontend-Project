96e1a5fe291ff60c51700814d847bd8326b99461
husnain572 <111906845+husnain572@users.noreply.github.com>
2023-08-04 19:44:42 +0500

    Add files via upload

---
+++ Ninestars/js/script.js
@@ -0,0 +1,43 @@
+//ScrollTop Button
+let btn_top=document.querySelector("#top-btn");
+window.onscroll=function(){
+    scrollFunction();
+}
+scrollFunction=()=>{
+if(document.body.scrollTop>20 || document.documentElement.scrollTop>20){
+    btn_top.style.display="block";
+}
+else{
+    btn_top.style.display="none";
+}
+}
+topFunction=()=>{
+    document.body.scrollTop=0;
+    document.documentElement.scrollTop=0;
+}
+
+// let sortBtn = document.querySelector('.filter-menu').children;
+// let sortItem = document.querySelector('.filter-item').children;
+
+// for(let i = 0; i < sortBtn.length; i++){
+//     sortBtn[i].addEventListener('click', function(){
+//         for(let j = 0; j< sortBtn.length; j++){
+//             sortBtn[j].classList.remove('current');
+//         }
+
+//         this.classList.add('current');
+        
+
+//         let targetData = this.getAttribute('data-target');
+
+//         for(let k = 0; k < sortItem.length; k++){
+//             sortItem[k].classList.remove('active');
+//             sortItem[k].classList.add('delete');
+
+//             if(sortItem[k].getAttribute('data-item') == targetData || targetData == "all"){
+//                 sortItem[k].classList.remove('delete');
+//                 sortItem[k].classList.add('active');
+//             }
+//         }
+//     });
+// }
